577d927bf9d47a53829e6f13bc016e99d76fcd50
jerin joseph <123jerin@gmail.com>
2021-03-02 17:17:38 +0530

    added source

---
+++ src/ToDo.js
@@ -0,0 +1,70 @@
+import React,{Component} from 'react';
+import './ToDo.css';
+
+
+class ToDo extends Component{
+
+    constructor(props){
+        super(props);
+        this.state={
+            items:[],
+            currText:""
+        }
+        
+    }
+
+    handleAdd = ()=>{
+        //alert("clicked")
+        let curritems=this.state.items;
+        curritems.push(this.state.currText);
+        this.setState({items:curritems});
+        console.log(this.state.items);
+        this.setState({currText:""})
+        document.getElementById("txtinput").value=""
+        
+    }
+
+    handleText = (e)=>{
+        //console.log(e.target.value);
+        this.setState({currText:e.target.value});
+    }
+
+    handleDelete = (k)=>{
+        if(!window.confirm("Are you Sure?")){
+            return;
+        }
+        let curritems=this.state.items;
+        curritems.splice(k,1);
+        this.setState({items:curritems})
+    }
+
+    render(){
+        
+
+        return(
+            <div>
+                <input type="text" id="txtinput" placeholder="WhatToDo" onChange={this.handleText}></input> <button onClick={this.handleAdd}>Add</button>
+            
+                <table>
+                    <tbody>
+                    { this.state.items.map((item,k)=>{
+                        return( 
+                            <tr>
+                                <td>{k+1}. {item} </td>
+                                <td> <button onClick={()=>{this.handleDelete(k)}}>Delete</button> </td> 
+                            </tr>
+                        
+                        )
+                    })}
+                    </tbody>
+                </table>
+                
+
+
+            </div>
+
+        );
+    }
+}
+
+export default ToDo;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ToDo from './ToDo'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ToDo/>,
   document.getElementById('root')
 );
 
