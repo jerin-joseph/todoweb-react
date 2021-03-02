3ba7ee1faaf3798528e40ff10c74ed9769563379
jerin joseph <123jerin@gmail.com>
2021-03-02 17:29:41 +0530

    added validation for input

--- src/ToDo.js
+++ src/ToDo.js
@@ -15,6 +15,11 @@ class ToDo extends Component{
 
     handleAdd = ()=>{
         //alert("clicked")
+        //alert(this.state.currText.trim().length)
+        if(this.state.currText.trim().length==0){
+            alert("Input cannot be empty. Try again.")
+            return;
+        }
         let curritems=this.state.items;
         curritems.push(this.state.currText);
         this.setState({items:curritems});
